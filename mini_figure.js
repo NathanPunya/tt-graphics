d0df2fde5f88a22bb75ee5aff4ec07f134e261b4
Henry Braid <henrybraid@gmail.com>
2025-02-27 00:32:50 -0800

    separated head and hair

--- mini_figure.js
+++ mini_figure.js
@@ -3,6 +3,7 @@ import { Shape_From_File } from './examples/obj-file-demo.js';
 const { vec3, vec4, color, Mat4, Shape, Material, Shader, Texture, Component } = tiny;
 
 const shapes = {
+    'hair': new Shape_From_File("lego_models/minifigure/hair/Untitled Model.obj"),
     'head': new Shape_From_File("lego_models/minifigure/head/Untitled Model.obj"),
     'body': new Shape_From_File("lego_models/minifigure/body/Untitled Model.obj"),
     'left_arm': new Shape_From_File("lego_models/minifigure/left_arm/Untitled Model.obj"),
@@ -18,17 +19,25 @@ const texturedPhong = new defs.Textured_Phong();
 const bumpmap = new defs.Fake_Bump_Map(1);
 const legoShader = new defs.Decal_Phong();
 
-const pantsColor = color(0.11953842797895521, 0.171441100722554, 0.26225065751888765, 1)
-const jacketColor = color(0.005181516700061659, 0.005181516700061659, 0.005181516700061659, 1)
-const skinColor = color(0.9386857284565036, 0.5711248294565854, 0.009134058699157796,1)
+const pantsColor = color(0.11953842797895521, 0.171441100722554, 0.26225065751888765, 1);
+const jacketColor = color(0.005181516700061659, 0.005181516700061659, 0.005181516700061659, 1);
+const skinColor = color(0.9386857284565036, 0.5711248294565854, 0.009134058699157796,1);
+const hairColor = color(0.18, 0.1, 0,1);
 
 const materials = {
+    hairMat:{
+        shader: legoShader,
+        ambient: 1,
+        diffusivity:1,
+        specularity:1,
+        color: hairColor,
+    },
     headMat: { shader: legoShader, 
         ambient: 1, 
         diffusivity: 1, 
         specularity: 1, 
-        texture: new Texture("lego_models/minifigure/head/textures/1/official/decoration/3626d1304.png"),
-        color: color(0,0,0,1)
+        texture: new Texture("lego_models/minifigure/head/textures/2/official/color/3626d220.png"),
+        color: skinColor
     },
     bodyMat: {shader: legoShader,
         ambient: 1,
@@ -90,10 +99,14 @@ class Mini_Figure {
         let scale = Mat4.scale(1, 1, 1);
         let origin = Mat4.translation(0, 0, 0);
 
-        const head_shape = shapes.head;
-        const head_transform = Mat4.scale(1, 1, 1).times(Mat4.translation(0, 5, 0));
-        this.head_node = new Node("head", head_shape, head_transform);
+        const hair_shape = shapes.hair;
+        const hair_transform = Mat4.scale(1.15,1.15,1.15).times(Mat4.translation(0,4.6,0));
+        this.hair_node = new Node("hair", hair_shape, hair_transform);
 
+        const head_shape = shapes.head;
+        const head_transform = Mat4.scale(0.98,0.98,0.98).times(Mat4.translation(0, 5.1, 0));
+        this.head_node = new Node("head", head_shape, head_transform);
+        
         const body_shape = shapes.body;
         const body_transform = Mat4.scale(1, 1, 1).times(Mat4.translation(0, 2.5, 0));
         this.body_node = new Node("body", body_shape, body_transform);
@@ -124,6 +137,7 @@ class Mini_Figure {
     }
 
     draw(webgl_manager, uniforms) {
+        this.hair_node.shape.draw(webgl_manager, uniforms, this.hair_node.transform_matrix, materials.hairMat);
         this.head_node.shape.draw(webgl_manager, uniforms, this.head_node.transform_matrix, materials.headMat);
         this.body_node.shape.draw(webgl_manager, uniforms, this.body_node.transform_matrix, materials.bodyMat);
         this.left_arm_node.shape.draw(webgl_manager, uniforms, this.left_arm_node.transform_matrix, materials.left_armMat);
