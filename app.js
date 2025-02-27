56fdacdf0308b0d75caa87c17a3d4f23fe5d7675
Henry Braid <henrybraid@gmail.com>
2025-02-26 20:42:35 -0800

    my version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { tiny, defs } from "./examples/common.js";
-const { vec3, vec4, color, Mat4, Shape, Shader, Component } = tiny;
+import { Shape_From_File } from "./examples/obj-file-demo.js";
+const { vec3, vec4, color, Mat4, Shape, Material, Shader, Texture, Component } = tiny;
 
 export const external = defs.external =
   class external extends Component {
@@ -7,6 +8,7 @@ export const external = defs.external =
       this.shapes = {
         box: new defs.Cube(),
         ball: new defs.Subdivision_Sphere(4),
+        greenBasePlate: new Shape_From_File('lego_models/greenBasePlate/greenBasePlate.obj')
       };
 
       const phong = new defs.Phong_Shader();
@@ -14,9 +16,10 @@ export const external = defs.external =
       this.materials = {
         plastic: { shader: phong, ambient: 0.2, diffusivity: 1, specularity: 0.5, color: color(0.9, 0.5, 0.9, 1) },
         metal: { shader: phong, ambient: 0.2, diffusivity: 1, specularity: 1, color: color(0.1, 0.9, 0.1, 1) },
+        lego: { shader: phong, ambient: 1, diffusivity: 1, specularity: 1, color: color( 0.007499032040460618, 0.20507873637973145, 0.019382360952473074,1)}
       };
       // Todo: Cool stuff starts here :D
-
+      
     }
 
     render_animation(caller) {
@@ -34,7 +37,7 @@ export const external = defs.external =
 
       //Lighting
       this.uniforms.projection_transform = Mat4.perspective(Math.PI / 4, caller.width / caller.height, 1, 100);
-      const lightPos = vec4(20, 20, 20, 1);
+      const lightPos = vec4(10, 10, 10, 1);
       this.uniforms.lights = [
         defs.Phong_Shader.light_source(lightPos, color(1, 1, 1, 1), 1e6)
       ];
@@ -50,11 +53,14 @@ export class main extends external {
 
     //Floor
     const groundColor = color(1, 0.7, 0, 1);
-    const floorXf = Mat4.translation(0, 0, 0).times(Mat4.scale(10, 0.01, 10));
+    /*const floorXf = Mat4.translation(0, 0, 0).times(Mat4.scale(10, 0.01, 10));
     this.shapes.box.draw(
       caller, this.uniforms, floorXf,
       { ...this.materials.plastic, color: groundColor }
-    );
+    );*/
+
+    const greenBasePlate_transform = Mat4.scale(10,10,10);
+    this.shapes.greenBasePlate.draw(caller, this.uniforms,greenBasePlate_transform, this.materials.lego);
   }
 
   render_controls() {
