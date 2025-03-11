3de1e553182957a0dbda2484a6982835d7a064d6
Henry Braid <henrybraid@gmail.com>
2025-03-11 16:45:18 -0700

    pieces now move to build the car. Might need to adjust the paths and spawn points of pieces though

--- build.js
+++ build.js
@@ -43,75 +43,100 @@ export class AnimateBuild{
     constructor(shape){
         this.shape = shape;
 
-        this.spline = new HermiteSpline();
+        this.splines = [];
+
+        this.animationStartTime =-1;
+        this.startTimes = [];
 
         this.generatePath();
     }
 
     generatePath(){
-        this.firstPoint = this.shape.nodes[0];
+        for(let i = 0; i<this.shape.nodes.length; i++){
+            let node = this.shape.nodes[i];
 
-        const M = this.firstPoint.transform_matrix;
-        const origin = vec4(0,0,0,1);
-        const position = M.times(origin); 
-        console.log(position);
+            this.splines[i] = new HermiteSpline();
 
-        const x = position[0];
-        const y = position[1];
-        const z = position[2];
+            const M = node.end_transform_matrix;
+            const origin = vec4(0,0,0,1);
+            const position = M.times(origin); 
 
-        const endPos = vec3(x, y, z);
+            const x = position[0];
+            const y = position[1];
+            const z = position[2];
+
+            const endPos = vec3(x, y, z);
+            
+            const xStart = getRandomInt(0, 2 * endPos[0]);
+            const zStart = getRandomInt(0, 2 *endPos[2]);
+
+            const startPos = vec3(xStart, 1, zStart);
+            node.setStartPos(startPos);
+
+            //Add Starting and End point to the spline:
+            let midpoint = vec3(0,0,0);
+            for(let i = 0; i<startPos.length; i++){
+                midpoint[i] = (startPos[i] + endPos[i])/2;
+            }
+            const variedIndex = getRandomInt(0, 2);
+            midpoint[variedIndex] *= 2; //make it curve sort of upwards/sideways in random direction
+            
+            this.splines[i].add_point(startPos[0], startPos[1], startPos[2], startPos[0]+1, startPos[1] + 1, startPos[2] -1);
+            this.splines[i].add_point(midpoint[0], midpoint[1], midpoint[2], midpoint[0] -1, midpoint[1] + 1, midpoint[2] -1);
+            this.splines[i].add_point(endPos[0], endPos[1], endPos[2], endPos[0] + 1, endPos[1] + 1, endPos[2] - 1);
+            
+            
+            //const curves = (t) => this.splines[i].get_position(t);
+            //this.curve = new Curve_Shape(curves, 1000, color(1,0,0,1));
+
+        }
+
         
-        //Make start point the origin
-        let originPos = vec3(x- x, y-y, z - z); //needs to be reset back to the origin so can properly find the position it needs to be in
-        console.log(originPos);
-        const startPos = vec3(originPos[0], originPos[1] + 1, originPos[2]);
-        this.firstPoint.setStartPos(startPos);
 
         // //confirm the transformation:
         // //Yes it is working, can comment this out
         // const position_after = this.firstPoint.start_transform_matrix.times(origin)
         // console.log(position_after);
 
-        //Add Starting and End point to the spline:
-        let midpoint = vec3(0,0,0);
-        for(let i = 0; i<startPos.length; i++){
-            midpoint[i] = (startPos[i] + endPos[i])/2;
-        }
-        const variedIndex = getRandomInt(0, 2);
-        midpoint[variedIndex] += 2;
         
-        
-        console.log(startPos);
-        this.spline.add_point(startPos[0], startPos[1], startPos[2], startPos[0]+1, startPos[1] + 1, startPos[2] -1);
-        this.spline.add_point(midpoint[0], midpoint[1], midpoint[2], midpoint[0] -1, midpoint[1] + 1, midpoint[2] -1);
-        this.spline.add_point(endPos[0], endPos[1], endPos[2], endPos[0] + 1, endPos[1] + 1, endPos[2] - 1);
-        
-        
-        const curves = (t) => this.spline.get_position(t);
-        this.curve = new Curve_Shape(curves, 1000, color(1,0,0,1));
-
     }
 
-    getPathTransform(uniforms){
-        const t = uniforms.animation_time / 1000;
-        let t_on_line = Math.min(t/4, 1);
+    getPathTransform(uniforms, index){
+        const global_t = uniforms.animation_time / 1000;
+        const timeDiff = global_t - this.startTimes[index];
+        let t_on_line;
+        if(global_t < this.startTimes[index]){
+            t_on_line = 0;
+        }else{
+            t_on_line = Math.min(timeDiff/4 , 1);
+        }
+        console.log(t_on_line);
+        let node = this.shape.nodes[index];
 
-        const M = this.firstPoint.transform_matrix;
+        const M = node.transform_matrix;
         const origin = vec4(0,0,0,1);
         const currentPos = M.times(origin); 
 
-        const pathPoint = this.spline.get_position(t_on_line);
+        const pathPoint = this.splines[index].get_position(t_on_line);
         const posChange = pathPoint.minus(currentPos);
 
-        this.firstPoint.setCurrentPos(posChange);
+        node.setCurrentPos(posChange);
 
-        return this.firstPoint.transform_matrix;
+        return node.transform_matrix;
     }
 
     drawPieces(caller, uniforms){
-        this.firstPoint.shape.draw(caller, uniforms, this.getPathTransform(uniforms), this.firstPoint.material);
-        this.curve.draw(caller, uniforms);
+        if(this.animationStartTime==-1){
+            this.animationStartTime = uniforms.animation_time / 1000;
+            for(let i = 0; i<this.shape.nodes.length; i++){
+                this.startTimes[i] = this.animationStartTime + 0.50 * i;
+            }
+        }
+        for(let i = 0; i<this.shape.nodes.length; i++){
+            let node = this.shape.nodes[i];
+            node.shape.draw(caller, uniforms, this.getPathTransform(uniforms, i), node.material);
+            //this.curve.draw(caller, uniforms);
+        }
     }
 
 }
